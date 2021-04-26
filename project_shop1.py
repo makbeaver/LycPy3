803ecfcd0cf68e3a095329e774ad1c302f1b1ab8
Max <50164468+makbeaver@users.noreply.github.com>
2021-04-26 09:24:03 +0300

    Модификация для heroku

--- project_shop1.py
+++ project_shop1.py
@@ -229,4 +229,5 @@ def create():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
